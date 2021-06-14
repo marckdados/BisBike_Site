49125f178f7730d4044e9cfb0e134fc70dffef3e
Mateus Ximenes <mateus.xas@hotmail.com>
2021-06-13 22:25:58 -0300

    new branch

--- post.js
+++ post.js
@@ -1,15 +1,48 @@
-function fazPost(url,body){
+function fazPostLogin(url,body){
     body = JSON.stringify(body)
     console.log("BODY: ", body)
     let request = new XMLHttpRequest();
+    
     request.open("POST",url,true);
     request.setRequestHeader("Content-type","application/json");
-    request.send(body)
-
-    request.onLoad = function(){
-        console.log(this.responseText)
-    }
-
+    let resultado = request.send(body)
+    console.log(resultado)
+    request.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+        if (this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            window.location.href = "home.html";
+             return data
+            // we get the returned data
+        }
+        if (this.status == 500) {
+            window.alert("Falha");          
+        }
+    };
+    
+    return request.responseText
+}
+function fazPostCadastro(url,body){
+    body = JSON.stringify(body)
+    console.log("BODY: ", body)
+    let request = new XMLHttpRequest();
+    
+    request.open("POST",url,true);
+    request.setRequestHeader("Content-type","application/json");
+    let resultado = request.send(body)
+    console.log(resultado)
+    request.onreadystatechange = function () {
+        if (this.readyState != 4) return;
+        if (this.status == 200) {
+            window.location.href = "home.html";
+             return data
+            // we get the returned data
+        }
+        if (this.status == 500) {
+            window.alert("Não foi possivel cadastrar este usuário");          
+        }
+    };
+    
     return request.responseText
 }
 function validarLogin(){
@@ -21,5 +54,19 @@ function validarLogin(){
         "usuario": userName,
         "senha": password 
      };
-    fazPost(url,body)
+     fazPostLogin(url,body)
+}
+function fazerCadastro(){
+    event.preventDefault()
+    let url = "https://bisbike-backend.herokuapp.com/usuario/cadastrar"
+    let userName = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    let body = {
+        "usuario": userName,
+        "senha": password ,
+        "email": email
+     };
+     fazPostCadastro(url,body);
+    
 }
